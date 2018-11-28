57e3f4b3bf20808e6b65b563fec2cb95045a2a3a
amdayton <amdayton@gmail.com>
2018-11-28 13:33:39 -0600

    Add OBJLoader example

--- js/App.js
+++ js/App.js
@@ -5,15 +5,15 @@ import { OBJLoader } from '@calvinscofield/three-loaders';
 
 // params used for the gui
 const _default_params = {
-  scale: 1.0
+  color: 0x00ff00
 };
 
 // costructor options
 const _default_options = {
-  clearColor: 0x000000,
-  clearAlpha: 1.0,
-  camFov: 45,
-  camPosition: new THREE.Vector3(0, 0, 10)
+  color: 0x00ff00,
+  camFov: 60,
+  camNear: 0.1,
+  camFar: 1000
 }
 
 
@@ -22,22 +22,56 @@ export default class App {
     this.params = Object.assign({}, _default_params, params);
     this.opts = Object.assign({}, _default_options, options);
 
-    // this.app =  new OrbitControls(camera, rendstatuserer.domElement)
-    this.geo = new THREE.TetrahedronGeometry();
-    this.mat = new THREE.MeshBasicMaterial();
-    this.mesh = new THREE.Mesh(geo, mat);
+    // set the scene
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
+      this.opts.camFov,
+      window.innerWidth / window.innerHeight,
+      this.opts.camNear,
+      this.opts.camFar
+    );
+    this.camera.position.z = 3;
 
-    this.app.scene.add(mesh);
+    // set up renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    // orbit controls
+    this.controls =  new OrbitControls(this.camera, this.renderer.domElement);
+
+    // .obj loader
+    this.loader = new OBJLoader();
+    this.loader.load('/assets/TV.obj', obj => {
+      this.tv = obj;
+      this.tv.traverse(child => {
+        if (child.isMesh) {
+          child.material = new THREE.MeshBasicMaterial({
+            color: this.params.color,
+            wireframe: true
+          });
+        }
+      });
+      this.scene.add(this.tv);
+      this.update();
+    });
+
+
 
     this.setupGui();
-    this.update();
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      this.renderer.render(this.scene, this.camera);
+    }
+    animate();
   }
 
   setupGui() {
     this.gui = new dat.GUI();
 
     // set gui controls
-    this.gui.add(this.params, 'scale', 0.1, 5.0);
+    this.gui.addColor(this.params, 'color');
 
     // automatically set listener for all params
     this.gui.__controllers.forEach((controller, index) => {
@@ -47,7 +81,13 @@ export default class App {
 
 
   update() {
-    this.mesh.scale(this.params.scale);
+    if(this.tv) {
+      this.tv.traverse(child => {
+        if (child.isMesh) {
+          child.material.color.set(this.params.color);
+        }
+      });
+    }
   }
 
 };
