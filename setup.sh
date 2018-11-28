d8b193ae3085ad77e24010e008e434634a2c23a9
amdayton <amdayton@gmail.com>
2018-11-28 10:17:55 -0600

    Fix sed issue

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ TEMPLATE_NAME="template-webpack-threejs"
 read -p "What is the name of your new project? (kabab-case)" name
 
 sed -i '' "s/$TEMPLATE_NAME/$name/g" package.json
-sed -i '' 's/^\s+"setup": "\./setup\.sh",$//g' package.json
+sed -i '' 's/^\s+"setup": "\.\/setup\.sh",$//g' package.json
 rm -rf ./.git
 git init
 yarn install
